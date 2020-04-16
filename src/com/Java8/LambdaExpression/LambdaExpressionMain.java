2f2f93d4b3a88855496739755b6ef9ef0d76b73e
abhishekjainiitg <abhishekjainiitg@gmail.com>
2020-04-17 01:29:30 +0530

    Move driver code from Main to LambdaExpressionMain

---
+++ src/com/Java8/LambdaExpression/LambdaExpressionMain.java
@@ -0,0 +1,43 @@
+package com.Java8.LambdaExpression;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaExpressionMain {
+    public void execute() {
+        // Filtering java files without lambda expression
+        FileFilterExample fileFilterExample = new FileFilterExample();
+        File[] javaFiles = fileFilterExample.getJavaFiles("D:\\Programming\\Java\\3JavaInteractiveCourses\\twitterDataAnalyzer\\src\\com\\twitter\\hasgtag");
+
+        for(File file : javaFiles) {
+            System.out.println(file.getName());
+        }
+
+        System.out.println("*********************************************************************************************");
+
+        // Filtering java files without lambda expression
+        File[] javaFilesLambda = fileFilterExample.getJavaFilesLambda("D:\\Programming\\Java\\3JavaInteractiveCourses\\twitterDataAnalyzer\\src\\com\\twitter\\hasgtag");
+
+        for(File file : javaFilesLambda) {
+            System.out.println(file.getName());
+        }
+
+        System.out.println("*********************************************************************************************");
+        // Sorting Java Lists using Anonymous class implementation of Comparator
+        ComparatorExample comparatorExample = new ComparatorExample();
+        List<String> list = Arrays.asList("***", "*", "****", "**", "******", "*****", "***");
+
+        comparatorExample.getSortedList(list);
+
+        System.out.println(list);
+
+        System.out.println("*********************************************************************************************");
+        // Sorting Java Lists using Lambda Expression implementation of Comparator
+        comparatorExample.getSortedListLambda(list);
+
+        System.out.println(list);
+
+        System.out.println("*********************************************************************************************");
+    }
+}
--- src/com/Java8/Main.java
+++ src/com/Java8/Main.java
@@ -2,6 +2,7 @@ package com.Java8;
 
 import com.Java8.LambdaExpression.ComparatorExample;
 import com.Java8.LambdaExpression.FileFilterExample;
+import com.Java8.LambdaExpression.LambdaExpressionMain;
 
 import java.io.File;
 import java.util.Arrays;
@@ -10,39 +11,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-
-        // Filtering java files without lambda expression
-        FileFilterExample fileFilterExample = new FileFilterExample();
-        File[] javaFiles = fileFilterExample.getJavaFiles("D:\\Programming\\Java\\3JavaInteractiveCourses\\twitterDataAnalyzer\\src\\com\\twitter\\hasgtag");
-
-        for(File file : javaFiles) {
-            System.out.println(file.getName());
-        }
-
-        System.out.println("*********************************************************************************************");
-
-        // Filtering java files without lambda expression
-        File[] javaFilesLambda = fileFilterExample.getJavaFilesLambda("D:\\Programming\\Java\\3JavaInteractiveCourses\\twitterDataAnalyzer\\src\\com\\twitter\\hasgtag");
-
-        for(File file : javaFilesLambda) {
-            System.out.println(file.getName());
-        }
-
-        System.out.println("*********************************************************************************************");
-        // Sorting Java Lists using Anonymous class implementation of Comparator
-        ComparatorExample comparatorExample = new ComparatorExample();
-        List<String> list = Arrays.asList("***", "*", "****", "**", "******", "*****", "***");
-
-        comparatorExample.getSortedList(list);
-
-        System.out.println(list);
-
-        System.out.println("*********************************************************************************************");
-        // Sorting Java Lists using Lambda Expression implementation of Comparator
-        comparatorExample.getSortedListLambda(list);
-
-        System.out.println(list);
-
-        System.out.println("*********************************************************************************************");
+        LambdaExpressionMain lambdaExpressionMain = new LambdaExpressionMain();
+        lambdaExpressionMain.execute();
     }
 }
